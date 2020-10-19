dc02f231d162cf21d6b61c4219096e216c73cfca
Ibrahim Mustafa <59354268+ibra-mustafa@users.noreply.github.com>
2020-10-19 15:29:16 +0200

    Update exercises.php

--- exercises.php
+++ exercises.php
@@ -1,4 +1,4 @@
- Exercise 1: Charlie bit my finger! (after lecture 3)
+ Exercise 1: Charlie bit my finger! 
 Part 1: Charlie will bite your finger exactly 50% of the time. First, write a function
 isBitten() that returns TRUE with 50% probability, and FALSE otherwise.
 Hint: You may find the rand() function useful.
